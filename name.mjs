298bf9947ad0f85ec5820b7bf5c17b9962de10c4
Ikiscream <33494335+ikii378@users.noreply.github.com>
2024-05-23 08:36:04 +0800

    Delete name.mjs

--- name.mjs
+++
@@ -1,4 +0,0 @@
-export const USERNAME = ["gesture reward oblige eight silk dance exile result abuse crunch guard barrel",
-"olive letter slot sustain market damp mixture hunt inherit chef world snack",
-"enforce slush genre utility hero plate trial federal badge random eager govern",
-  ];
